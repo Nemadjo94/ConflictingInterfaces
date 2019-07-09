e4cb4ea19f7ca6dd3286c4093a36a8f0ad9baf76
Nemanja <nemadjo94@gmail.com>
2019-07-09 10:52:11 +0200

    Add files via upload

---
+++ ConflictingInterfaces/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConflictingInterfaces
+{
+    class Program
+    {
+        //Imamo dva interfejsa koja imaju isti naziv metode
+        //Klasa koja nasledjuje ta dva interfejsa prihvata taj metod
+        //Ali u zavisnosti koji interfejs se instancira ta se metoda poziva
+        static void Main(string[] args)
+        {
+            
+            TestInterface1 obj1 = new TestClass();
+            TestInterface2 obj2 = new TestClass();
+            TestClass obj3 = new TestClass();
+
+            obj1.Show();//Izvrsava metodu iz TestInterface1
+            obj2.Show();//Izvrsava metodu iz TestInterface2
+            obj3.Show();//Izvrsava se za klasu
+
+           
+
+        }
+    }
+}
---
+++ ConflictingInterfaces/TestClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConflictingInterfaces
+{
+    class TestClass : TestInterface1, TestInterface2
+    {
+        //Zajednicka metoda za oba TestInterfejsa
+        public void Show()
+        {
+            Console.WriteLine("For both of TestInterface");
+        }
+
+        //Ne mora se implementovati svaki interfejs posebno osim ako ne zelimo drugariji output za svaku metodu
+        //Jer u metoda iznad je zajednicka i za interfejse i za klasu
+        //Samo zavisi kako se instancira klasa ili interfejs
+        //To jest metoda se poziva iz svog tipa, ako je interfejs1 pozvace se za interfejs1 itd... 
+
+        void TestInterface1.Show()
+        {
+            Console.WriteLine("For TestInterface1");
+        }
+
+        void TestInterface2.Show()
+        {
+            Console.WriteLine("For TestInterface2");
+        }
+    }
+}
